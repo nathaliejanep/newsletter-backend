0d7e6b9c62f81c7625017b3f5adf2d87eabf03b2
Nathalie Persson <nathalie.persson@hotmail.com>
2022-05-25 11:09:18 +0200

    Added logout and remove cookies

--- routes/admin-route.js
+++ routes/admin-route.js
@@ -4,37 +4,6 @@ const fs = require('fs');
 const { authUser } = require('../auth');
 const UserModel = require('../models/user-model');
 
-router.get('/users', authUser, async (req, res) => {
-  let users = await UserModel.find();
-
-  let printSubscribers = '<div><h2>Subscribers</h2>';
-  let printUsers = '<div><h2>Users</h2>';
-  users.forEach((user) => {
-    printUsers += `
-    <ul>
-      <li>${user.username} </li>
-      <li>${user.newsletter}</li>
-    </ul>
-    </div>
-    `;
-
-    if (user.newsletter === true) {
-      printSubscribers += `
-        <ul>
-          <li>${user.username} </li>
-          <li>${user.newsletter}</li>
-        </ul>
-        </div>
-        `;
-    }
-  });
-
-  let printLists = printSubscribers;
-  printLists += printUsers;
-
-  res.status(200).send(printLists);
-});
-
 router.post('/login', (req, res) => {
   res.cookie('session_id', '123456');
 
@@ -50,11 +19,47 @@ router.post('/login', (req, res) => {
     });
 
     if (foundAdmin) {
-      return res.send('loggedin');
+      return res.redirect('/admin/users');
     }
 
     res.send('fel');
   });
+});
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('session_id');
+  res.redirect('/');
+});
+
+router.get('/users', authUser, async (req, res) => {
+  let users = await UserModel.find();
+
+  let printSubscribers =
+    '<a href="/admin/logout">Log out</a><h2>Subscribers</h2>';
+
+  let printUsers = '<h2>All Users</h2>';
+  users.forEach((user) => {
+    printUsers += `
+    <ul>
+      <li>Email: ${user.username} </li>
+      <li>Subscribes: ${user.newsletter ? 'Yes' : 'No'}</li>
+    </ul>
+    `;
+
+    if (user.newsletter === true) {
+      printSubscribers += `
+        <ul>
+          <li>${user.username} </li>
+          <li>${user.newsletter}</li>
+        </ul>
+        `;
+    }
+  });
+
+  let printLists = printSubscribers;
+  printLists += printUsers;
+
+  res.status(200).send(printLists);
 });
 
 module.exports = router;
